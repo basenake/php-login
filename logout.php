299d4f658dcf833594e7f8a9b8225d151bb7cae1
wayne <carzy70203@gmail.com>
2023-05-03 11:27:45 +0800

    session 設定

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+echo "以登出使用者" .$_SESSION['name'];
+
+unset($_SESSION['name']);
+
+
+?>
--- name.php
+++ name.php
@@ -1,8 +1,9 @@
 <?php
 
-
-
-echo $_COOKIE['name'];
+// echo $_COOKIE['name'];
+//sessionstart 後 session才可以使用
+session_start();
+echo $_SESSION['name'];
 
 
 
